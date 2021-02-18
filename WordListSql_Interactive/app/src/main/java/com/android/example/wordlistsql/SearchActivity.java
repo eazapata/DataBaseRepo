25650d57c081c66ce3bcafcfe0f38fd9cae63077
Eduardo Antonio Zapata Valero <eazapatavalero@gmail.com>
2021-02-18 13:34:00 +0100

    Task 2 complete

---
+++ WordListSql_Interactive/app/src/main/java/com/android/example/wordlistsql/SearchActivity.java
@@ -0,0 +1,47 @@
+package com.android.example.wordlistsql;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+
+
+    private static final String TAG = EditWordActivity.class.getSimpleName();
+    private TextView mTextView;
+    private EditText mEditWordView;
+    private WordListOpenHelper mDB;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        mEditWordView = ((EditText) findViewById(R.id.edit_word_search));
+        mTextView = ((TextView) findViewById(R.id.search_result));
+        mDB = new WordListOpenHelper(this);
+    }
+
+    public void showResult(View v) {
+        String word = mEditWordView.getText().toString();
+        mTextView.setText("Result for " + word + ":\n\n");
+        Cursor cursor = mDB.search(word);
+        if (cursor != null & cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            int index;
+            String result;
+            do {
+                index = cursor.getColumnIndex(WordListOpenHelper.KEY_WORD);
+                result = cursor.getString(index);
+                mTextView.append(result + "\n");
+            } while (cursor.moveToNext());
+            cursor.close();
+        }else{
+            mTextView.setText("No results for the search of "+word);
+        }
+    }
+}
+
